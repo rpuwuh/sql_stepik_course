28c760946913c6dfbb5f5c18b12cbe2e5c97fdb1
mdgrishin <mdgrishin@hex.team>
2024-11-20 21:58:51 +0300

    added 1 exercise of 2.2

--- 2/2.2.sql
+++ 2/2.2.sql
@@ -54,3 +54,19 @@ FROM
 WHERE genre.name_genre LIKE '%роман%'
 ORDER BY title
 ;
+
+-- Посчитать количество экземпляров  книг каждого автора из таблицы author.
+-- Вывести тех авторов,  количество книг которых меньше 10,
+-- в отсортированном по возрастанию количества виде.
+-- Последний столбец назвать Количество.
+
+SELECT name_author, sum(amount) AS Количество
+FROM 
+    author LEFT JOIN book
+    on author.author_id = book.author_id
+GROUP BY name_author
+HAVING
+    sum(amount) < 10
+    OR COUNT(title) = 0
+ORDER BY sum(amount)
+;  
