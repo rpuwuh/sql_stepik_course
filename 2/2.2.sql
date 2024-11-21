e8effeb7676bef1579937b33a12de7948a177e48
mdgrishin <mdgrishin@hex.team>
2024-11-21 10:21:07 +0300

    added another exercise of 2.2 module

--- 2/2.2.sql
+++ 2/2.2.sql
@@ -70,3 +70,24 @@ HAVING
     OR COUNT(title) = 0
 ORDER BY sum(amount)
 ;  
+
+-- Вывести в алфавитном порядке всех авторов,
+-- которые пишут только в одном жанре.
+-- Поскольку у нас в таблицах так занесены данные,
+-- что у каждого автора книги только в одном жанре,
+-- для этого запроса внесем изменения в таблицу book.
+-- Пусть у нас  книга Есенина «Черный человек» относится к жанру «Роман»,
+-- а книга Булгакова «Белая гвардия» к «Приключениям»
+-- (эти изменения в таблицы уже внесены).
+
+SELECT name_author
+FROM 
+    author INNER JOIN book
+    on author.author_id = book.author_id
+    AND author.author_id IN (
+            SELECT author_id
+            FROM book GROUP BY author_id
+            HAVING COUNT(DISTINCT genre_id) = 1
+          )
+GROUP BY name_author
+;
