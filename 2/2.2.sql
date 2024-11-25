6af45e70bd4bf54239d974883cae9cdee34b2707
mdgrishin <mdgrishin@hex.team>
2024-11-25 11:59:44 +0300

    added more exercises of 2.2 module

--- 2/2.2.sql
+++ 2/2.2.sql
@@ -91,3 +91,55 @@ FROM
           )
 GROUP BY name_author
 ;
+
+-- Вывести информацию о книгах (название книги, фамилию и инициалы автора,
+-- название жанра, цену и количество экземпляров книги),
+-- написанных в самых популярных жанрах, в отсортированном
+-- в алфавитном порядке по названию книг виде.
+-- Самым популярным считать жанр,
+-- общее количество экземпляров книг которого на складе максимально.
+
+SELECT title, name_author, name_genre, price, amount
+FROM 
+    author 
+    INNER JOIN book ON author.author_id = book.author_id
+    INNER JOIN genre ON  book.genre_id = genre.genre_id
+GROUP BY title, name_author,name_genre, book.genre_id, price, amount
+HAVING book.genre_id IN
+         (/* выбираем автора, если он пишет книги в самых популярных жанрах*/
+          SELECT query_in_1.genre_id
+          FROM 
+              ( /* выбираем код жанра и количество произведений, относящихся к нему */
+                SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+               )query_in_1
+          INNER JOIN 
+              ( /* выбираем запись, в которой указан код жанр с максимальным количеством книг */
+                SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+                ORDER BY sum_amount DESC
+                LIMIT 1
+               ) query_in_2
+          ON query_in_1.sum_amount= query_in_2.sum_amount
+         )
+ORDER BY title
+;
+
+-- Если в таблицах supply  и book есть одинаковые книги,
+-- которые имеют равную цену,  вывести их название и автора,
+-- а также посчитать общее количество экземпляров книг
+--в таблицах supply и book,  столбцы назвать Название, Автор  и Количество.
+
+SELECT
+    book.title AS 'Название',
+    name_author AS 'Автор',
+    (supply.amount + book.amount) AS 'Количество'
+FROM
+    book 
+    INNER JOIN author USING (author_id)   
+    INNER JOIN supply ON book.title = supply.title 
+                         and author.name_author = supply.author
+                         and book.price = supply.price
+;
